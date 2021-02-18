b71bfc97ac3e8f601bdd64ac351b252da5b8adf9
DawidWebb <tomasz.matras@developerweb.pl>
2021-02-18 10:21:25 +0100

    fix: modify ManSection

--- src/vievs/MainSection/MainSection.js
+++ src/vievs/MainSection/MainSection.js
@@ -1,37 +1,16 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
 import { StoreContext } from "../../Store/StoreProvider";
 
-import SelectButton from "../../components/Buttons/SelectButton/SelectButton";
-import LoginForm from "../LoginForm/LoginForm";
+import WelcomeSite from "../WelcomeSite/WelcomeSite";
+import LoggedMenu from "../LoggedMenu/LoggedMenu";
+
 import styles from "./MainSection.module.scss";
 
 function MainSection() {
-  const { loginModalOpen, setLoginModalOpen } = useContext(StoreContext);
+  const { user } = useContext(StoreContext);
 
-  const handleOpenLoginModal = () => {
-    setLoginModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setLoginModalOpen(false);
-  };
-  return (
-    <div className={styles.container}>
-      <div className={styles.loginApp}>
-        <SelectButton name={"logowanie"} onClick={handleOpenLoginModal} />
-        <LoginForm
-          isModalOpen={loginModalOpen}
-          handleCloseModal={handleCloseModal}
-        />
-      </div>
-      <div className={styles.testApp}>
-        <Link to={"/test-form"}>
-          <SelectButton name={"testuj"} />
-        </Link>
-      </div>
-    </div>
-  );
+  const mainViev = user ? <LoggedMenu /> : <WelcomeSite />;
+  return <>{mainViev}</>;
 }
 
 export default MainSection;
---
+++ src/vievs/WelcomeSite/WelcomeSite.js
@@ -0,0 +1,9 @@
+const WelcomeSite = () => {
+  return (
+    <div>
+      <h1>Welcome App</h1>
+    </div>
+  );
+};
+
+export default WelcomeSite;
