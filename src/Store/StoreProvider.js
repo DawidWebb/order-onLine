3f80fc3db99cc921295fa04c29ac9e71013bc799
DawidWebb <tomasz.matras@developerweb.pl>
2021-02-18 10:19:02 +0100

    feat: add helpers-request, add States to Store

--- src/Store/StoreProvider.js
+++ src/Store/StoreProvider.js
@@ -3,10 +3,16 @@ import React, { createContext, useEffect, useState } from "react";
 export const StoreContext = createContext(null);
 
 const StoreProvider = ({ children }) => {
-  //Modal Rodo Viev
+  // login Modal viev
   const [loginModalOpen, setLoginModalOpen] = useState(false);
+
+  // user login
+  const [user, setUser] = useState(null);
+
   return (
-    <StoreContext.Provider value={{ loginModalOpen, setLoginModalOpen }}>
+    <StoreContext.Provider
+      value={{ loginModalOpen, setLoginModalOpen, user, setUser }}
+    >
       {children}
     </StoreContext.Provider>
   );
