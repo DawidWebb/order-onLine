56b5f7af62a4e1b3b6923e0d98a2ae9c4486c321
DawidWebb <tomasz.matras@developerweb.pl>
2021-11-18 11:53:07 +0100

    fix print viev, add describes

--- src/helpers/companyData.js
+++ src/helpers/companyData.js
@@ -1,8 +1,9 @@
 export const COMPANY_DATA = [
-  " Termin płatności wynosi 45 dni i jest liczony od daty wpływu FV Zleceniobiorcy, wraz z dokumentami dotyczącymi tras zawartych w FV.",
-  "Skrócony termin płatności: skonto 5%, płatność w 48h po otrzymaniu FV wraz z kompletem dokumentów przewozowych. W przypadku skonta FV powinna zostać pomniejszona o 5%, opis: 'FV pomniejszona o 5% ze względu na wcześniejszą płatność'",
+  "Termin płatności jest liczony od daty wpływu FV zleceniobiorcy, wraz z dokumentami dotyczącymi tras zawartych w FV.",
+  "Skrócony termin płatności: skonto 5%, płatność w 48h po otrzymaniu FV wraz z kompletem dokumentów przewozowych. W przypadku skonta FV powinna zostać pomniejszona o 5%, opis: 'FV pomniejszona o 5% ze względu na wcześniejszą płatność'.",
+  "Informujemy że nie akceptujemy faktur na których, na których skonto udzeilane jest przez zewnętrzne podmioty.",
   "Frachty naliczone w walucie Euro: płatne w Euro na jedno podane konto walutowe.",
-  " Transport krajowy wykonywany w opaciu o: Ustawa o Prawie Przewozowym i OWS.",
+  "Transport krajowy wykonywany w opaciu o: Ustawa o Prawie Przewozowym i OWS.",
   "Transport międzynarodowy wykonywany w opaciu o:: konwencja CMR.",
 ];
 export const OWNED_COMPANY = {
